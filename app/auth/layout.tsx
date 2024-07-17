56bbeb2054887697dd3489db2ccdf037679824f3
K1chel <cherevko022@gmail.com>
2024-07-17 17:13:36 -0400

    protect auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,8 +1,15 @@
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
 type Props = {
   children: React.ReactNode;
 };
 
-const AuthLayout = ({ children }: Props) => {
+const AuthLayout = async ({ children }: Props) => {
+  const session = await auth();
+
+  if (session) redirect("/dashboard");
+
   return (
     <div className="h-screen flex items-center justify-center">{children}</div>
   );
